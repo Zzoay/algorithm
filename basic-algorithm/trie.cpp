93d59fb56f0e0643fba315df6a9d080e0b37b896
Yaozz <jianggongyao@gmail.com>
2021-03-10 20:28:15 +0800

    add trie

---
+++ basic-algorithm/trie.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Trie{
+
+private:
+  bool isEnd;
+  vector<Trie *> next;
+
+public:
+  Trie(){
+    isEnd = false;
+    next = vector<Trie *>(26, nullptr);
+  }
+
+  void insert(string word){
+    Trie *node = this;
+
+    for(char c : word){
+      int idx = c - 'a';
+      if(node->next[idx] == NULL){
+        node->next[idx] = new Trie();
+      }
+      node = node->next[idx]; 
+    }
+    node->isEnd = true;
+  }
+
+  bool search(string word){
+    Trie *node = this;
+
+    for(char c : word){
+      node = node->next[c - 'a'];
+      if(node == NULL) return false;
+    }
+    return node->isEnd;
+  }
+
+  bool isStartWith(string prefix){
+    Trie *node = this;
+
+    for(char c : prefix){
+      node = node->next[c - 'a'];
+      if(node == NULL) return false;
+    }
+    return true;
+  }
+};
+
+
+int main(){
+  Trie t;
+  t.insert("and");
+  cout << t.search("and") << endl;
+  cout << t.search("ad") << endl;
+  
+  cout << t.isStartWith("an") << endl;
+  cout << t.isStartWith("ad") << endl;
+}
